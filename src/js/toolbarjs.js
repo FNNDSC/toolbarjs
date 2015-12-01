7d5462c037c754fa34cbafdff956a0d9513a9da0
Jorge <jbernal0019@yahoo.es>
2015-12-01 11:01:18 -0500

    refactored code

--- src/js/toolbarjs.js
+++ src/js/toolbarjs.js
@@ -16,21 +16,33 @@ define(['jquery_ui'], function() {
     * Class implementing the tool bar
     *
     * @constructor
-    * @param {Object} toolbar's options with properties: contId, position.
+    * @param {Object} toolbar's options with properties:
+    *   -container: toolbar's container's DOM id or DOM object
+    *   -position: toolbar's css position object with possible properties top, bottom, left, right
     */
     toolbarjs.ToolBar = function(options) {
 
       this.version = 0.0;
-      // toolbar container's ID
-      this.contId = options.contId;
+
+      // toolbar's container
+      if (typeof options.container === 'string') {
+
+        // a DOM id was passed
+        this.container = $('#' + options.container);
+
+      } else {
+
+        // a DOM object was passed
+        this.container = $(options.container);
+      }
+
       // toolbar's css position object with possible properties top, bottom, left, right
       if (options.position) {
         this.position = options.position;
       } else {
         this.position = {};
       }
-      // jQuery object for the bar's div element (tool bar container)
-      this.jqToolBar = null;
+
       // associative array of button event handlers
       this.eventHandlers = null;
     };
@@ -40,16 +52,8 @@ define(['jquery_ui'], function() {
      */
      toolbarjs.ToolBar.prototype.init = function() {
 
-       // return if tool bar already initialized
-       if (this.jqToolBar) {
-         return;
-       }
-
-       // set jQuery obj for the tool bar
-       this.jqToolBar = $('#' + this.contId);
-
        // add the appropriate classes
-       this.jqToolBar.addClass("view-toolbar");
+       this.container.addClass("view-toolbar");
 
        // initialize array of button event handlers
        this.eventHandlers = {};
@@ -63,37 +67,37 @@ define(['jquery_ui'], function() {
      * @param {Object} css position object with possible properties: "top", "bottom", "left" and "right".
      */
      toolbarjs.ToolBar.prototype.setPosition = function(pos) {
-       var jqToolBar = this.jqToolBar;
+       var container = this.container;
        var t = "", r = "", b = "", l = "";
 
        if (pos) {
 
          if (pos.top) {
            this.position.top = pos.top;
-           jqToolBar.css({ top: pos.top });
+           container.css({ top: pos.top });
            t = ' - ' + pos.top;
          }
 
          if (pos.right) {
            this.position.right = pos.right;
-           jqToolBar.css({ right: pos.right });
+           container.css({ right: pos.right });
            r = ' - ' + pos.right;
          }
 
          if (pos.bottom) {
            this.position.bottom = pos.bottom;
-           jqToolBar.css({ bottom: pos.bottom });
+           container.css({ bottom: pos.bottom });
            b = ' - ' + pos.bottom;
          }
 
          if (pos.left) {
            this.position.left = pos.left;
-           jqToolBar.css({ left: pos.left });
+           container.css({ left: pos.left });
            l = ' - ' + pos.left;
          }
 
          if (r || l) {
-           jqToolBar.css({ width: 'calc(100%' + r + l + ')' });
+           container.css({ width: 'calc(100%' + r + l + ')' });
          }
        }
      };
@@ -104,10 +108,9 @@ define(['jquery_ui'], function() {
      * @param {Object} object containing button's properties: id (HTML id), title, caption, onclick
      */
      toolbarjs.ToolBar.prototype.addButton = function(btnProps) {
-       var jqToolBar = this.jqToolBar;
 
        // append a new button to the tool bar
-       jqToolBar.append(
+       this.container.append(
          '<button id="' + btnProps.id + '" class="view-toolbar-button" type="button" title="' +
           btnProps.title + '">' + btnProps.caption + '</button>'
        );
@@ -160,8 +163,8 @@ define(['jquery_ui'], function() {
      toolbarjs.ToolBar.prototype.destroy = function() {
 
        this.eventHandlers = null;
-       this.jqToolBar.empty();
-       this.jqToolBar = null;
+       this.container.empty();
+       this.container = null;
      };
 
 
